da9291513236750cfc3f0281ee0243c91788d253
Ifeanyi Obiano <ifeanyiobiano@Ifeanyis-MacBook-Pro.local>
2019-05-05 23:58:21 +0200

    add card-flip styling

--- src/Pokecard/pokecard.js
+++ src/Pokecard/pokecard.js
@@ -1,24 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import './pokecard.css';
 
 // const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
-const padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3): num);
+const padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3) : num);
 
 class Pokecard extends Component {
-    render() {
-        const imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
-        return (
-            <div className="Pokecard">
-                <h1 className="Pokecard-title" >{this.props.name}</h1>
-                <div className="Pokecard-image">
-                    <img src={imgSrc} alt={this.props.name} />
-                </div>
-                <div className="Pokecard-data">Exp: {this.props.exp} </div>
-                <div className="Pokecard-data">Type: {this.props.type} </div>
-            </div>
-        );
-    } 
+	render() {
+		const imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+		return (
+			<div className="flip-card">
+				<div className="flip-card-inner">
+					<div className="Pokecard-front">
+						<h1 className="Pokecard-title">Guess</h1>
+						<div className="Pokecard-image">
+							<h1>Pokemon Card</h1>
+						</div>
+						<div className="Pokecard-data">Exp: {this.props.exp} </div>
+						<div className="Pokecard-data">Type: {this.props.type} </div>
+					</div>
+					<div className="Pokecard-back">
+						<h1 className="Pokecard-title">{this.props.name}</h1>
+						<div className="Pokecard-image">
+							<img src={imgSrc} alt={this.props.name} />
+						</div>
+						<div className="Pokecard-data">Exp: {this.props.exp} </div>
+						<div className="Pokecard-data">Type: {this.props.type} </div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 export default Pokecard;
