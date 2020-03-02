bf390fd6e41104e04931d47a83f23b60caa194bd
Ifeanyi Obiano <ifeanyiobiano@Ifeanyis-MacBook-Pro-2.local>
2020-03-02 07:54:18 +0100

    Code refactor to function components with hooks instead of class components

--- src/Pokecard/pokecard.js
+++ src/Pokecard/pokecard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './pokecard.css';
 
 // const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
@@ -6,59 +6,68 @@ const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 const padToThree = (num) => (num <= 999 ? `00${num}`.slice(-3) : num);
 
-class Pokecard extends Component {
-	handleFlipCard = () => {
-		if (this.props.flipCardAction) return;
-		this.props.flipCard({ uniqueId: this.props.uniqueId, name: this.props.name });
+const Pokecard = ({
+	flipCardAction,
+	flipCard,
+	uniqueId,
+	name,
+	imageId,
+	isCurrentName,
+	isPrevName,
+	isMatched,
+	exp,
+	type
+}) => {
+	const handleFlipCard = () => {
+		if (flipCardAction) return;
+		flipCard({ uniqueId: uniqueId, name: name });
 	};
 
-	render() {
-		const imgSrc = `${POKE_API}${padToThree(this.props.imageId)}.png`;
-		const stayOpen = this.props.isCurrentName && this.props.isPrevName;
-		const flipCard = this.props.flipCardAction || stayOpen || this.props.isMatched ? ' flip-card-action' : '';
-		return (
-			<div className={`flip-card${flipCard}`}>
-				<div className="flip-card-inner">
-					<div className="Pokecard-front" onClick={this.handleFlipCard}>
-						<h1 className="Pokecard-title">Guess</h1>
-						<div className="Pokecard-image">
-							<h1>Pokemon Card</h1>
-						</div>
-						<div className="Pokecard-data">
-							<span role="img" aria-label="Monser">
-								👾👾👾
-							</span>
-						</div>
-						<div className="Pokecard-data">
-							<span role="img" aria-label="Diamonds">
-								♦️♦️️♦️♦️
-							</span>
-						</div>
+	const imgSrc = `${POKE_API}${padToThree(imageId)}.png`;
+	const stayOpen = isCurrentName && isPrevName;
+	const showCard = flipCardAction || stayOpen || isMatched ? ' flip-card-action' : '';
+	return (
+		<div className={`flip-card${showCard}`}>
+			<div className="flip-card-inner">
+				<div className="Pokecard-front" onClick={handleFlipCard}>
+					<h1 className="Pokecard-title">Guess</h1>
+					<div className="Pokecard-image">
+						<h1>Pokemon Card</h1>
 					</div>
-					<div className="Pokecard-back">
-						{flipCard ? (
-							<React.Fragment>
-								<h1 className="Pokecard-title">{this.props.name}</h1>
-								<div className="Pokecard-image">
-									<img src={imgSrc} alt={this.props.name} />
-								</div>
-								<div className="Pokecard-data">Exp: {this.props.exp} </div>
-								<div className="Pokecard-data">Type: {this.props.type} </div>
-							</React.Fragment>
-						) : (
-							<React.Fragment>
-								<h1 className="Pokecard-title">Dummy</h1>
-								<div className="Pokecard-image">
-									<img src={imgSrc} alt="Demo" />
-								</div>
-								<div className="Pokecard-data">Dummy</div>
-								<div className="Pokecard-data">Dummy</div>
-							</React.Fragment>
-						)}
+					<div className="Pokecard-data">
+						<span role="img" aria-label="Monser">
+							👾👾👾
+						</span>
+					</div>
+					<div className="Pokecard-data">
+						<span role="img" aria-label="Diamonds">
+							♦️♦️️♦️♦️
+						</span>
 					</div>
 				</div>
+				<div className="Pokecard-back">
+					{flipCard ? (
+						<React.Fragment>
+							<h1 className="Pokecard-title">{name}</h1>
+							<div className="Pokecard-image">
+								<img src={imgSrc} alt={name} />
+							</div>
+							<div className="Pokecard-data">Exp: {exp} </div>
+							<div className="Pokecard-data">Type: {type} </div>
+						</React.Fragment>
+					) : (
+						<React.Fragment>
+							<h1 className="Pokecard-title">Dummy</h1>
+							<div className="Pokecard-image">
+								<img src={imgSrc} alt="Demo" />
+							</div>
+							<div className="Pokecard-data">Dummy</div>
+							<div className="Pokecard-data">Dummy</div>
+						</React.Fragment>
+					)}
+				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 export default Pokecard;
--- src/Pokedex/pokedex.js
+++ src/Pokedex/pokedex.js
@@ -1,65 +1,66 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './pokedex.css';
 import Pokecard from '../Pokecard/pokecard';
 
-class Pokedex extends Component {
-	constructor() {
-		super();
-		this.state = {
-			matchedCards: {},
-			currentUniqueId: '',
-			prevUniqueId: '',
-			currentName: '',
-			prevName: ''
-		};
-	}
+const Pokedex = ({ pokemon }) => {
+	const initialState = {
+		matchedCards: {},
+		currentUniqueId: '',
+		prevUniqueId: '',
+		currentName: '',
+		prevName: ''
+	};
+	const [ cardStatus, setCardStatus ] = useState(initialState);
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevState.currentName === this.state.currentName && !this.state.matchedCards[this.state.currentName]) {
-			const matchedCard = {
-				[this.state.currentName]: true
-			};
-			this.setState({
-				matchedCards: Object.assign(this.state.matchedCards, matchedCard)
-			});
-		}
-	}
+	useEffect(
+		() => {
+			if (cardStatus.prevName === cardStatus.currentName) {
+				const matchedCard = {
+					[cardStatus.currentName]: true
+				};
+				setCardStatus({
+					...cardStatus,
+					matchedCards: Object.assign(cardStatus.matchedCards, matchedCard)
+				});
+			}
+		},
+		[ cardStatus ]
+	);
 
-	flipCard = ({ uniqueId, name }) => {
-		this.setState({
+	const onCardflipHandler = ({ uniqueId, name }) => {
+		setCardStatus({
+			...cardStatus,
 			currentUniqueId: uniqueId,
-			prevUniqueId: this.state.currentUniqueId,
+			prevUniqueId: cardStatus.currentUniqueId,
 			currentName: name,
-			prevName: this.state.currentName
+			prevName: cardStatus.currentName
 		});
 	};
 
-	render() {
-		return (
-			<div>
-				<h1 className="Pokedex-winner">Pokemon</h1>
-				<p>Memory Game</p>
-				<div className="Pokedex-cards">
-					{this.props.pokemon.map((p) => (
-						<Pokecard
-							imageId={p.image_id}
-							key={p.uniqueId}
-							uniqueId={p.uniqueId}
-							name={p.name}
-							type={p.type}
-							exp={p.base_experience}
-							matchedCards={this.state.matchedCards}
-							flipCard={this.flipCard}
-							flipCardAction={this.state.currentUniqueId === p.uniqueId}
-							isCurrentName={this.state.currentName === p.name}
-							isPrevName={this.state.prevName === p.name}
-							isMatched={this.state.matchedCards[p.name]}
-						/>
-					))}
-				</div>
+	return (
+		<div>
+			<h1 className="Pokedex-winner">Pokemon</h1>
+			<p>Memory Game</p>
+			<div className="Pokedex-cards">
+				{pokemon.map((p) => (
+					<Pokecard
+						imageId={p.image_id}
+						key={p.uniqueId}
+						uniqueId={p.uniqueId}
+						name={p.name}
+						type={p.type}
+						exp={p.base_experience}
+						matchedCards={cardStatus.matchedCards}
+						flipCard={onCardflipHandler}
+						flipCardAction={cardStatus.currentUniqueId === p.uniqueId}
+						isCurrentName={cardStatus.currentName === p.name}
+						isPrevName={cardStatus.prevName === p.name}
+						isMatched={cardStatus.matchedCards[p.name]}
+					/>
+				))}
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Pokedex;
